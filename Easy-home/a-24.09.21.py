47ca123dcf17f3fa39058e49f6d6aa60fec9ace7
Gora <it.is.mr.lag@gmail.com>
2021-09-24 19:17:39 +0500

    первое дз)

---
+++ Easy-home/a-24.09.21.py
@@ -0,0 +1,10 @@
+# №5
+
+n = int(input('Number for №5: '))
+for n in range(n):
+    print(150 + n, end='')
+print('')
+
+# №12
+
+print(sum([i*i for i in range(101)]))
--- Easy-home/t-24.09.21.py
+++ Easy-home/t-24.09.21.py
@@ -1,3 +1,26 @@
+# №1 -------------------------------------- #
+
+print(input('Number for №1: '))
+
+# №2 -------------------------------------- #
+
+print(max(int(input('First num: ')), int(input('Second num: '))))
+
+# №3 -------------------------------------- #
+
+n = int(input('Number for №3: '))
+if n > 5:
+    print(n * 2)
+else:
+    print(n - 7)
+
+# №4 -------------------------------------- #
+
+n = int(input('Number for №4: '))
+for n in range(n):
+    print(1 + n, end='')
+print('')
+
 # №21 -------------------------------------- #
 
 n = int(input('Number for №21: '))
